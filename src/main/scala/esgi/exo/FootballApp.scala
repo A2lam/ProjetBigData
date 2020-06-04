b80b98490b935a9db88f84fae7a1919b3e3bc6b6
HADJI Mohamed Allam <mohamedallam31@hotmail.com>
2020-06-04 23:37:42 +0200

    Implemented 2d DF with total games, avg France goals and avg Other goals

--- src/main/scala/esgi/exo/FootballApp.scala
+++ src/main/scala/esgi/exo/FootballApp.scala
@@ -4,6 +4,10 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions.col
 import org.apache.spark.sql.functions.to_date
 import org.apache.spark.sql.functions.udf
+import org.apache.spark.sql.functions.count
+import org.apache.spark.sql.functions.countDistinct
+import org.apache.spark.sql.functions.lit
+import org.apache.spark.sql.functions.avg
 
 object FootballApp {
   // Function that holds int casting
@@ -12,6 +16,12 @@ object FootballApp {
       return 0
 
     Integer.valueOf(strInt)
+  }
+
+  // Function that holds home - away checking
+  def homeAwayChecking(game: String): Boolean = {
+    val homeTeam = game.split(" ")
+    homeTeam(0) == "France"
   }
 
   // Main program
@@ -23,23 +33,31 @@ object FootballApp {
       .config("spark.master", "local")
       .getOrCreate()
 
+    // Checking if the file path was submitted
+    if (args.length == 0) {
+      println("Veuillez soumettre le chemin du fichier en argument")
+
+      // Stopping the spark session
+      spark.stop()
+    }
+
     // Reading the the csv input file into dataFrame
     val dfMatches = spark
       .read
       .option("header", value = true)
       .option("inferSchema", "true")
-      .csv("input/df_matches.csv")
+      .csv(args(0))
 
-    // Turning it into UDF
+    // Declaring extract integer UDF
     val extractInteger = udf(this.convertToInt _)
 
-    // Selecting wanted columns and casting
     val colScoreFrance = col("score_france")
     val colScoreAdversaire = col("score_adversaire")
     val colPenaltyFrance = col("penalty_france")
     val colPenaltyAdversaire = col("penalty_adversaire")
     val colDate = col("date")
 
+    // Selecting wanted columns and casting
     val dfCasted = dfMatches
       .select("X4", "X6", "adversaire", "score_france", "score_adversaire", "penalty_france", "penalty_adversaire", "date")
       .withColumn("score_france_casted", extractInteger(colScoreFrance))
@@ -62,8 +80,32 @@ object FootballApp {
     // Keeping only matches above 1980
     val dfMatchesFranceAfter1980 = dfMatchesFrance.filter(colDate >= "1980-03-01")
 
-    dfMatchesFranceAfter1980.printSchema()
-    dfMatchesFranceAfter1980.show(3)
+    // Declaring check home - away UDF
+    val homeAwayCheck = udf(this.homeAwayChecking _)
+
+    val colMatch = col("match")
+    val colAdversaire = col("adversaire")
+    val colHomeMatch = col("match_a_domicile")
+
+    // Adding home - away checking column
+    val dfMatchesFranceHA = dfMatchesFranceAfter1980.withColumn("match_a_domicile", homeAwayCheck(colMatch))
+
+    // Printing result df schema and some rows
+    dfMatchesFranceHA.printSchema()
+    dfMatchesFranceHA.show(10)
+
+    // Calculating stats
+    val dfStats = dfMatchesFranceHA
+      .groupBy(colAdversaire)
+      .agg(
+        count(lit(1)).alias("total_match"),
+        avg(colScoreFrance).alias("points_moyen_france"),
+        avg(colScoreAdversaire).alias("points_moyen_adversaire"),
+      )
+
+    // Printing some rows of result df
+    dfStats.show(10)
+    dfStats.write.mode("overwrite").parquet("data/stats.parquets")
 
     // Stopping the spark session
     spark.stop()
