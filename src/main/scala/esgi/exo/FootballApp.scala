157c24e005d01d8347ce45ce002a204af0868901
HADJI Mohamed Allam <mohamedallam31@hotmail.com>
2020-06-04 16:49:54 +0200

    Refactored snack to camel case

--- src/main/scala/esgi/exo/FootballApp.scala
+++ src/main/scala/esgi/exo/FootballApp.scala
@@ -4,14 +4,10 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions.col
 import org.apache.spark.sql.functions.to_date
 import org.apache.spark.sql.functions.udf
-import org.apache.spark
-
-import java.text.SimpleDateFormat
-import java.util.Date
 
 object FootballApp {
   // Function that holds int casting
-  def hold_integers(strInt: String): Integer = {
+  def convertToInt(strInt: String): Integer = {
     if (strInt == "NA")
       return 0
 
@@ -28,33 +24,33 @@ object FootballApp {
       .getOrCreate()
 
     // Reading the the csv input file into dataFrame
-    val df_matches = spark
+    val dfMatches = spark
       .read
       .option("header", value = true)
       .option("inferSchema", "true")
       .csv("input/df_matches.csv")
 
     // Turning it into UDF
-    val extract_integer = udf(this.hold_integers _)
+    val extractInteger = udf(this.convertToInt _)
 
     // Selecting wanted columns and casting
-    val col_score_france = col("score_france")
-    val col_score_adversaire = col("score_adversaire")
-    val col_penalty_france = col("penalty_france")
-    val col_penalty_adversaire = col("penalty_adversaire")
-    val col_date = col("date")
+    val colScoreFrance = col("score_france")
+    val colScoreAdversaire = col("score_adversaire")
+    val colPenaltyFrance = col("penalty_france")
+    val colPenaltyAdversaire = col("penalty_adversaire")
+    val colDate = col("date")
 
-    val df_casted = df_matches
+    val dfCasted = dfMatches
       .select("X4", "X6", "adversaire", "score_france", "score_adversaire", "penalty_france", "penalty_adversaire", "date")
-      .withColumn("score_france_casted", extract_integer(col_score_france))
-      .withColumn("score_adversaire_casted", extract_integer(col_score_adversaire))
-      .withColumn("penalty_france_casted", extract_integer(col_penalty_france))
-      .withColumn("penalty_adversaire_casted", extract_integer(col_penalty_adversaire))
-      .withColumn("date_casted", to_date(col_date,"yyyy-MM-dd"))
+      .withColumn("score_france_casted", extractInteger(colScoreFrance))
+      .withColumn("score_adversaire_casted", extractInteger(colScoreAdversaire))
+      .withColumn("penalty_france_casted", extractInteger(colPenaltyFrance))
+      .withColumn("penalty_adversaire_casted", extractInteger(colPenaltyAdversaire))
+      .withColumn("date_casted", to_date(colDate,"yyyy-MM-dd"))
       .drop("score_france", "score_adversaire", "penalty_france", "penalty_adversaire", "date")
 
     // Renaming some columns
-    val df_matches_france = df_casted
+    val dfMatchesFrance = dfCasted
       .withColumnRenamed("X4", "match")
       .withColumnRenamed("X6", "competition")
       .withColumnRenamed("score_france_casted", "score_france")
@@ -64,10 +60,10 @@ object FootballApp {
       .withColumnRenamed("date_casted", "date")
 
     // Keeping only matches above 1980
-    val df_matches_france_after_1980 = df_matches_france.filter(col_date >= "1980-03-01")
+    val dfMatchesFranceAfter1980 = dfMatchesFrance.filter(colDate >= "1980-03-01")
 
-    df_matches_france_after_1980.printSchema()
-    df_matches_france_after_1980.show(3)
+    dfMatchesFranceAfter1980.printSchema()
+    dfMatchesFranceAfter1980.show(3)
 
     // Stopping the spark session
     spark.stop()
